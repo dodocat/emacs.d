d40282cf6e91fa9006a133905c652f79dec123fa
Jing Quanqi <jingquanqi@gmail.com>
2012-11-01 17:38:07 +0800

    a lot of changes

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(background-color "#002b36")
+ '(background-mode dark)
+ '(coffee-tab-width 2)
+ '(css-indent-offset 2)
+ '(cursor-color "#839496")
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(foreground-color "#839496")
+ '(js-indent-level 2)
+ '(zencoding-indentation 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
